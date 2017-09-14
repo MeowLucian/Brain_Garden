82268ceb0de84fd8850af538cf3f0b2ae2e46b53
MeowLucian <Meow.Lucian@gmail.com>
2017-09-14 20:17:01 +0800

    In App URLs

--- Django_web/Brain_Garden/urls.py
+++ Django_web/Brain_Garden/urls.py
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
-from posts.views import post_home
+from posts.views import post_create, post_list, post_detail, post_update, post_delete
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^posts/$', post_home),
+    url(r'^posts/', include("posts.urls")),
 ]
---
+++ Django_web/posts/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from django.contrib import admin
+from posts.views import post_create, post_list, post_detail, post_update, post_delete
+
+urlpatterns = [
+    url(r'^$', post_list),  # Posts url Default
+    url(r'^create/$', post_create),
+    url(r'^list/$', post_list),
+    url(r'^detail/$', post_detail),
+    url(r'^update/$', post_update),
+    url(r'^delete/$', post_delete),
+]
