57b85121b850fef95e4f19d1c640f637a840d73e
MeowLucian <Meow.Lucian@gmail.com>
2017-09-17 23:53:18 +0800

    QuerySet Basics

--- Django_web/posts/views.py
+++ Django_web/posts/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 
 def post_create(request):
@@ -13,7 +13,9 @@ def post_detail(request):
     return render(request, "index.html", context)
 
 def post_list(request):
+    queryset = Post.objects.all()
     context = {
+        "object_list": queryset,
         "title": "List"
     }
     return render(request, "index.html", context)
