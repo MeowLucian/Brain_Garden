24d309075bd560d2368c3d6cbc11b152fc843b61
MeowLucian <Meow.Lucian@gmail.com>
2017-09-27 20:04:14 +0800

    Delete View

--- Django_web/posts/urls.py
+++ Django_web/posts/urls.py
@@ -3,10 +3,10 @@ from django.contrib import admin
 from posts.views import post_create, post_list, post_detail, post_update, post_delete
 
 urlpatterns = [
-    url(r'^$', post_list),  # Posts url Default
+    url(r'^$', post_list, name='list'),  # Posts url Default
     url(r'^create/$', post_create),
     url(r'^list/$', post_list),
     url(r'^(?P<id>\d+)/$', post_detail, name='detail'),
     url(r'^(?P<id>\d+)/edit/$', post_update, name='update'),
-    url(r'^delete/$', post_delete),
+    url(r'^(?P<id>\d+)/delete/$', post_delete),
 ]
