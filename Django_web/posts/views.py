4162db515a21bb29d51c3e7e1d806d4f6eb568bf
MeowLucian <Meow.Lucian@gmail.com>
2017-09-28 22:31:04 +0800

    Templates & Inheritance

--- Django_web/posts/views.py
+++ Django_web/posts/views.py
@@ -12,8 +12,6 @@ def post_create(request):
         # message success
         messages.success(request, "Successfully Created")
         return HttpResponseRedirect(instance.get_absolute_url())
-    else:
-        messages.error(request, "Not Successfully Created")
 
     context = {
         "form": form,
@@ -34,7 +32,7 @@ def post_list(request):
         "object_list": queryset,
         "title": "List"
     }
-    return render(request, "index.html", context)
+    return render(request, "post_list.html", context)
 
 def post_update(request, id=None):
     instance = get_object_or_404(Post, id=id)
